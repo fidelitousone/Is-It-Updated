0ecad3d18acef6eb3f74eae424785b2e05c26ba7
Jatinder Singh <js843@njit.edu>
2021-01-15 14:42:18 -0500

    allow bot to be enabled for use

--- IsItUpdated.py
+++ IsItUpdated.py
@@ -1,6 +1,17 @@
+from dotenv import load_dotenv
+import os
+import discord
+
+load_dotenv()
+
+CLIENT_ID = os.getenv("CLIENT_ID")
+
 def main():
     print("Hello World!")
+    print(f"Discord Client ID: {CLIENT_ID}")
 
 
 if (__name__ == '__main__'):
     main()
+    client = discord.Client()
+    client.run(CLIENT_ID)
